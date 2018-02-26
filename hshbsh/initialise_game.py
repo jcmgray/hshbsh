90b57c0823323ff3469ae6ff4983d93109a28a7c
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2018-02-26 23:29:10 +0000

    initialise_slice is almost done. test to be done

--- hshbsh/initialise_game.py
+++ hshbsh/initialise_game.py
@@ -1,6 +1,8 @@
-#import slice_lib as sl
+import slice_lib as sl
 import numpy as np
 
+# initialising the owner
+#
 # pizza = array of array n X n
 # min_tomato = l
 # max_size = h
@@ -11,6 +13,7 @@ def initialise_owner(pizza, min_tomato, max_size, size_init_slice = [2,2] ):
 	nrow, ncol = pizza.shape
 	ID_slices = 1
 	owner = np.zeros(pizza.shape, int)
+	slice_info = []
 
 	init_slice = np.zeros(size_init_slice, int)
 
@@ -22,10 +25,24 @@ def initialise_owner(pizza, min_tomato, max_size, size_init_slice = [2,2] ):
 			start_i = i*size_init_slice[0]
 			start_j = j*size_init_slice[1]
 			owner[start_i:start_i+size_init_slice[0],start_j:start_j+size_init_slice[1]] = ID_slices
+			slice_info.append([ID_slices, start_i, start_i+size_init_slice[0]-1, start_j, start_j+size_init_slice[1]-1])
 			ID_slices += 1
 
 	owner.resize( (nrow, ncol) )
 
-	return owner
+	return owner, slice_info
 
-#def initialise_slice
+# initialising the slice
+#
+# pizza = array of array n X n
+# owner = array of array n X n
+# slices = something?
+
+def initialise_slice(pizza, owner, slice_info, slices):
+
+	slice_dictionary = {}
+
+	for identity, ri, rf, ci, cf in slice_info:
+		slice_dictionary[identity] = sl.Slice(pizza, owner, slices, identity, ri, rf, ci, cf)
+
+	return slice_dictionary
--- tests/test_hello_lib.py
+++ tests/test_hello_lib.py
@@ -12,7 +12,7 @@ def test_hello(name):
 def test_initialise_owner_perfect_size():
 
 	pizza = np.array([[0,1,1,1],[0,0,1,1],[0,1,1,1],[0,0,1,1]], int)
-	test_owner = initialise_game.initialise_owner( pizza, 2, 5 )
+	test_owner, excess = initialise_game.initialise_owner( pizza, 2, 5 )
 	expected_owner = np.array([[1,1,2,2],[1,1,2,2],[3,3,4,4],[3,3,4,4]], int)
 
 	assert_allclose( test_owner, expected_owner )
@@ -20,7 +20,7 @@ def test_initialise_owner_perfect_size():
 def test_initialise_owner_shit_size():
 
 	pizza = np.array([[0,1,1,1,0],[0,0,1,1,1],[0,0,1,1,1],[0,1,0,1,1],[0,0,1,1,1]], int)
-	test_owner = initialise_game.initialise_owner( pizza, 2, 5 )
+	test_owner, excess = initialise_game.initialise_owner( pizza, 2, 5 )
 	expected_owner = np.array([[1,1,2,2,0],[1,1,2,2,0],[3,3,4,4,0],[3,3,4,4,0],[0,0,0,0,0]], int)
 
 	assert_allclose( test_owner, expected_owner )
@@ -28,7 +28,26 @@ def test_initialise_owner_shit_size():
 def test_initialise_owner_wrong():
 
 	pizza = np.array([[0,1,1,1,0],[0,0,1,1,1],[0,0,1,1,1],[0,1,0,1,1],[0,0,1,1,1]], int)
-	test_owner = initialise_game.initialise_owner( pizza, 2, 5 )
+	test_owner, excess = initialise_game.initialise_owner( pizza, 2, 5 )
 	expected_owner = np.array([[1,1,2,2,7],[1,1,2,2,0],[3,3,4,4,0],[3,3,4,4,0],[0,0,0,0,0]], int)
 
 	assert not np.allclose( test_owner, expected_owner )
+
+def test_initialise_slice_info_perfect_size():
+
+	pizza = np.array([[0,1,1,1],[0,0,1,1],[0,1,1,1],[0,0,1,1]], int)
+	excess, slice_info = initialise_game.initialise_owner( pizza, 2, 5 )
+	expected_slice_info = [[1,0,1,0,1],[2,0,1,2,3],[3,2,3,0,1],[4,2,3,2,3]]
+
+	assert_allclose( np.array(slice_info), np.array(expected_slice_info) )
+
+def test_initialise_slice_correct():
+	
+	pizza = np.array([[0,1,1,1],[0,0,1,1],[0,1,1,1],[0,0,1,1]], int)
+	owner = np.array([[1,1,2,2,0],[1,1,2,2,0],[3,3,4,4,0],[3,3,4,4,0],[0,0,0,0,0]], int)
+	slice_info = [[1,0,1,0,1],[2,0,1,2,3],[3,2,3,0,1],[4,2,3,2,3]]
+	slices = 'what is this'
+
+	expected_dict = initialise_game.initialise_slice(pizza, owner, slice_info, slices)
+
+	print(expected_dict)
