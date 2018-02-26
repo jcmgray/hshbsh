cf6a0aa206ac284e21d66c3e683a45553c6d8ae1
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2018-02-26 22:41:59 +0000

    initialise_owner is done and works (tests done)

--- hshbsh/initialise_game.py
+++ hshbsh/initialise_game.py
@@ -1,4 +1,4 @@
-import slice_lib as sl
+#import slice_lib as sl
 import numpy as np
 
 # pizza = array of array n X n
@@ -8,10 +8,9 @@ import numpy as np
 
 def initialise_owner(pizza, min_tomato, max_size, size_init_slice = [2,2] ):
 
-	nrow, ncol = pizza.shape()
+	nrow, ncol = pizza.shape
 	ID_slices = 1
-	owner_row = np.empty()
-	owner = np.empty()
+	owner = np.zeros(pizza.shape, int)
 
 	init_slice = np.zeros(size_init_slice, int)
 
@@ -20,11 +19,13 @@ def initialise_owner(pizza, min_tomato, max_size, size_init_slice = [2,2] ):
 
 	for i in range(number_slice_x):
 		for j in range(number_slice_y):
-			init_slice.fill(ID_slices)
-			owner_row = np.concatenate( (owner_row, init_slice), axis = 1 )
-			ID_slice += 1
-		owner = np.concatenate( (owner, owner_row), axis = 0)
+			start_i = i*size_init_slice[0]
+			start_j = j*size_init_slice[1]
+			owner[start_i:start_i+size_init_slice[0],start_j:start_j+size_init_slice[1]] = ID_slices
+			ID_slices += 1
 
-	print(owner)
+	owner.resize( (nrow, ncol) )
 
+	return owner
 
+#def initialise_slice
