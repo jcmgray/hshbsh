258b4f9cacf635b715457cbcbce1bdd2fa4b3f94
Johnnie Gray <johnniemcgray@gmail.com>
2018-03-01 21:15:47 +0000

    wide option

--- hshbsh/main.py
+++ hshbsh/main.py
@@ -3,13 +3,20 @@ from .output import write_journeys
 from .taxi_lib import Taxi
 
 
-def main_function(fname, outname='answer.txt'):
+def main_function(fname, outname='answer.txt', wide=True):
     settings, journeys = load_data(fname)
 
     taxis = [Taxi() for _ in range(settings.ntaxi)]
 
-    any_valid = True
-    while any_valid:
-        any_valid = all(taxi.do_closest(journeys) for taxi in taxis)
+    if wide:
+        any_valid = True
+        while any_valid:
+            any_valid = all(taxi.do_closest(journeys) for taxi in taxis)
+
+    else:
+        for taxi in taxis:
+            has_valid_journey = True
+            while has_valid_journey:
+                has_valid_journey = taxi.do_closest(journeys)
 
     write_journeys(taxis, outfilename=outname)
