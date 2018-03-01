c28cc2c872335a46a87158db96951038f5cf479f
Tom Melt <thomas.meltzer1@gmail.com>
2018-03-01 19:06:16 +0000

    write function complete -- needs testing with actual class

--- hshbsh/output.py
+++ hshbsh/output.py
@@ -1,8 +1,17 @@
+from hshbsh import Taxi
 
 def writeJournies(taxis):
-    for taxi in taxis:
-
-        print(taxi)
+    outfilename="answer.txt"
+    with open(outfilename,"w") as outfile:
+        for i,taxi in enumerate(taxis):
+            taxiID=i+1
+            numberOfJourneys=len(taxi)
+            journeys=taxi
+            output=[taxiID,numberOfJourneys]+journeys
+            output=[str(x) for x in output]
+            outputString=" ".join(output)+"\n"
+            print(outputString,end="")
+            outfile.write(outputString)
     return
 
-if __name__ == __main__ : writeJournies()
+if __name__ == "__main__" : writeJournies([[0,1,2],[3,4,5],[6,7,8,9,10]])
