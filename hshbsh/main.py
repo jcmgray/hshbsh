891fd545d15bb830768cbd3e28f18c13fe0ded1a
Johnnie Gray <johnniemcgray@gmail.com>
2018-03-01 21:09:41 +0000

    taxi breadth first

--- hshbsh/main.py
+++ hshbsh/main.py
@@ -3,15 +3,13 @@ from .output import write_journeys
 from .taxi_lib import Taxi
 
 
-def main_function(fname, outname):
+def main_function(fname, outname='answer.txt'):
     settings, journeys = load_data(fname)
 
     taxis = [Taxi() for _ in range(settings.ntaxi)]
 
-    for taxi in taxis:
-
-        has_valid_journey = True
-        while has_valid_journey:
-            has_valid_journey = taxi.do_closest(journeys)
+    any_valid = True
+    while any_valid:
+        any_valid = all(taxi.do_closest(journeys) for taxi in taxis)
 
     write_journeys(taxis, outfilename=outname)
