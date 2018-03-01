c202fbe92dc0e29590f25b73106741139781130f
Tom Melt <thomas.meltzer1@gmail.com>
2018-03-01 19:56:20 +0000

    making test

--- tests/testOutput.py
+++ tests/testOutput.py
@@ -1,6 +1,14 @@
 import pytest
-from hshbsh import output as out
+from hshbsh.output import write_journeys
+from hshbsh import Taxi
+
+taxi1=Taxi()
+taxi2=Taxi()
+taxi1.jids=[2,4,6]
+taxi2.jids=[1,3,5]
 
 def checkOutput():
-    taxis = 
-    return
+    taxis=[taxi1,taxi2]
+    output=write_journeys(taxis,"test",False)
+    print(output)
+    assert 1 == 2
